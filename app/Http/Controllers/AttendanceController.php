b9ff25e0c91d87a5ab61464ea5e139f5bcafe181
ilman Manarul Qori <ilmanmanarulqori@ilmans-MacBook-Pro-2.local>
2020-09-01 00:29:27 +0700

    add face recognition

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -39,7 +39,7 @@ class AttendanceController extends Controller
     }
 
     public function setAttendance() {
-
+        return response()->json(request()->all());
         $now = Carbon::now();
         $now->addHours(7);
         $data = null;
@@ -173,34 +173,5 @@ class AttendanceController extends Controller
         }
         
         return response()->json($data);
-    }
-
-    public function http_request($url, $data){
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $data,
-            CURLOPT_HTTPHEADER => array(
-                "user_id: 5f3bc90b5bab1b52bae885e8",
-            ),
-        ));
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-            return $err;
-        } else {
-            return $response;
-        }
     }
 }
