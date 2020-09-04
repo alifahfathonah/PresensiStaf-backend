030c5636dc5697ea8e7523196afc5c818fe0817f
ilman Manarul Qori <ilmanmanarulqori@Syauqis-MBP.localdomain>
2020-09-04 13:10:11 +0700

    update approve Sick

--- app/Http/Controllers/SickController.php
+++ app/Http/Controllers/SickController.php
@@ -8,6 +8,9 @@ use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use App\User;
 use App\Sick;
+use App\Days;
+use App\Attendance;
+use App\Schedule;
 use Yajra\DataTables\Datatables;
 
 class SickController extends Controller
@@ -60,6 +63,29 @@ class SickController extends Controller
         $sick->date_sick = json_encode($dates);
         $sick->amount = count($dates);
         $sick->status = request()->status;
+
+        if(request()->status == 'approved'){
+            foreach($dates as $date){
+                $dateSick = Carbon::parse($date);
+                $getDays = Days::where('name_day', $dateSick->format('l'))->first();
+                $getSchedule = Schedule::where('users_id', $sick->users_id)->where('days_id', $getDays->id)->first();
+                
+                if($getSchedule){ // jika ada jadwalnya, maka create
+                    Attendance::create([
+                        "user_id" => $sick->users_id,
+                        // "user_id" => 1,
+                        "start" => Carbon::parse($date.' '.$getSchedule->clock_in)->addHour(7),
+                        "end" => Carbon::parse($date.' '.$getSchedule->clock_out)->addHour(7),
+                        "is_on_area" => "0",
+                        "hours" => $getSchedule->hours,
+                        "note_start" => '',
+                        "note_end" => '',
+                        "status" => 'sakit'
+                    ]);
+                }
+            }
+        }
+
         $sick->is_sick_letter = 1;
         $sick->note = request()->note;
         $sick->request_to = 1;
@@ -143,6 +169,57 @@ class SickController extends Controller
         $sick->date_sick = json_encode($dates);
         $sick->amount = count($dates);
         $sick->status = request()->status;
+
+        // bersihkan data sebelumnya
+        $sickOld = Sick::findOrFail($id);
+        foreach(json_decode($sickOld->date_sick) as $date){
+            $dateSick = Carbon::parse($date);
+            $getDays = Days::where('name_day', $dateSick->format('l'))->first();
+            $getSchedule = Schedule::where('users_id', $sick->users_id)->where('days_id', $getDays->id)->first();
+            
+            if($getSchedule){ // jika ada jadwalnya, maka create
+                $dataAttendance = Attendance::whereDate('start',$date)->where('user_id', $sick->users_id)->first();
+                if($dataAttendance){
+                    $dataAttendance->delete();
+                }
+            }
+        }
+
+        if(request()->status == 'approved'){
+            foreach($dates as $date){
+                $dateSick = Carbon::parse($date);
+                $getDays = Days::where('name_day', $dateSick->format('l'))->first();
+                $getSchedule = Schedule::where('users_id', $sick->users_id)->where('days_id', $getDays->id)->first();
+                
+                if($getSchedule){ // jika ada jadwalnya, maka create
+                    Attendance::create([
+                        "user_id" => $sick->users_id,
+                        // "user_id" => 1,
+                        "start" => Carbon::parse($date.' '.$getSchedule->clock_in)->addHour(7),
+                        "end" => Carbon::parse($date.' '.$getSchedule->clock_out)->addHour(7),
+                        "is_on_area" => "0",
+                        "hours" => $getSchedule->hours,
+                        "note_start" => '',
+                        "note_end" => '',
+                        "status" => 'sakit'
+                    ]);
+                }
+            }
+        } else {
+            foreach($dates as $date){
+                $dateSick = Carbon::parse($date);
+                $getDays = Days::where('name_day', $dateSick->format('l'))->first();
+                $getSchedule = Schedule::where('users_id', $sick->users_id)->where('days_id', $getDays->id)->first();
+                
+                if($getSchedule){ // jika ada jadwalnya, maka create
+                    $dataAttend = Attendance::whereDate('start',$date)->where('user_id', $sick->users_id)->first();
+                    if($dataAttend){
+                        $dataAttend->delete();
+                    }
+                }
+            }
+        }
+
         $sick->is_sick_letter = 1;
         $sick->note = request()->note;
         $sick->request_to = 1;
@@ -183,6 +260,20 @@ class SickController extends Controller
     public function destroy($id)
     {
         $data = Sick::findOrFail($id);
+
+        foreach(json_decode($data->date_sick) as $date){
+            $dateSick = Carbon::parse($date);
+            $getDays = Days::where('name_day', $dateSick->format('l'))->first();
+            $getSchedule = Schedule::where('users_id', $data->users_id)->where('days_id', $getDays->id)->first();
+            
+            if($getSchedule){ // jika ada jadwalnya, maka delete
+                $dataAttend = Attendance::whereDate('start',$date)->where('user_id', $data->users_id)->first();
+                if($dataAttend){
+                    $dataAttend->delete();
+                }
+            }
+        }
+
         $data->delete();
 
         return redirect()->route('sick.index')->with('success','Data berhasil dihapus!');
@@ -208,7 +299,6 @@ class SickController extends Controller
                     return date('d-m-Y', strtotime($item->created_at));
                 })
                 ->addColumn('action', function($item){
-                    if($item->status == 'pending') {
                         return 
                         // '<a href="#" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-eye-open"></i> Show</a> '.
                         '<a href="'.route("sick.edit", $item->id).'" class="mr-2"><svg viewBox="0 0 24 24" width="18" height="18" stroke="#ffc107" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a> '.
@@ -226,7 +316,7 @@ class SickController extends Controller
                                 event.preventDefault();
                         }" 
                         class=""><svg viewBox="0 0 24 24" width="18" height="18" stroke="#dc3545" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>';
-                    }
+                    
                 })->rawColumns(['action'])->make(true);
     }
 }
