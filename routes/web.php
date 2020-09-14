997b42e4a1b8879a88472d8f6170ca32f83f748f
ilman Manarul Qori <ilmanmanarulqori@ilmans-MacBook-Pro-2.local>
2020-09-14 11:30:56 +0700

    upload apk release

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,32 @@
 |
 */
 
+use Symfony\Component\HttpFoundation\Response;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('download/{filename}', function($filename)
+{
+    // Check if file exists in app/storage/file folder
+    $file_path = asset('../public/file/'.$filename);
+    $download_link = link_to_asset('file/'.$filename);
+    return $download_link;
+    if (file_exists($file_path))
+    {
+        // Send Download
+        return Response::download($file_path, $filename, [
+            'Content-Length: '. filesize($file_path)
+        ]);
+    }
+    else
+    {
+        // Error
+        exit('Requested file does not exist on our server!');
+    }
+})
+->where('filename', '[A-Za-z0-9\-\_\.]+');
 
 Auth::routes();
 
