46d8b4917637b11dc8bdcaf65945822fe5656a0d
ilman Manarul Qori <ilmanmanarulqori@ilmans-MacBook-Pro-2.local>
2020-10-16 07:52:03 +0700

    edit btn logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,8 +73,10 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                <a id="navbarDropdown" class="nav-link" href="{{ route('logout') }}" role="button" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                    {{-- {{ Auth::user()->name }} <span class="caret"></span> --}}
+                                    {{ __('Logout') }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
